46f38b56e9641d01c59dd2e14d3cf210a71d9f39
Deine-Mutder <konstantin.kunisch.dw@gmail.com>
2026-06-19 15:38:33 +0200

    update

--- responsive-login-form-with-gsap/assets/js/data.js
+++ responsive-login-form-with-gsap/assets/js/data.js
@@ -15,7 +15,7 @@ const USER_DATA = {
       password: 'bormann26',
       displayName: 'Konstantin',
       role: 'admin',
-      birthdate: '16.05.2010',
+      birthdate: '16.05.201000000',
       children: 'Nein',
       married: 'Nein',
    },
